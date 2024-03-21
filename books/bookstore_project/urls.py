91d3817744af7aafb34d960ea45b626f4b041069
bs1462 <jobaer.ahamed@brainstation-23.com>
2024-03-21 13:32:31 +0600

    add user registration

--- books/bookstore_project/urls.py
+++ books/bookstore_project/urls.py
@@ -19,5 +19,11 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    # local
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
